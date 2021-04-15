9ac7beea8bd388fab527495fb82e7c351b4f8523
Sepehr Roshanpour <sepehrroshanpour@gmail.com>
2021-04-15 15:27:11 +0430

    Loading Data in React

--- src/App.js
+++ src/App.js
@@ -1,64 +1,23 @@
 import "./App.css";
-import * as d3 from "d3";
-// import { useState, useCallback } from "react";
+// import { useCallback } from "react";
+import { useState, useEffect } from "react";
 // import { Face } from "./Face";
 // import { range, svg } from "d3";
+import { csv } from "d3";
+import { message } from "./message";
 
 // const array = range(9 * 3);
 
-// const width = 960;
-// const height = 500;
-// const circleRadius = 30;
-// const initialMousePosition = { x: width / 2, y: height / 2 };
-
 // export const App = () => array.map(() => <Face />);
-
-// export const App = () => {
-//   const [mousePosition, setMousePosition] = useState(initialMousePosition);
-//   const handleMouseMove = useCallback(
-//     (event) => {
-//       const { clientX, clientY } = event;
-//       setMousePosition({ x: clientX, y: clientY });
-//     },
-//     [setMousePosition]
-//   );
-//   return (
-//     <svg width={width} height={height} onMouseMove={handleMouseMove}>
-//       <circle
-//         cx={mousePosition.x}
-//         cy={mousePosition.y}
-//         r={circleRadius}
-//       ></circle>
-//     </svg>
-//   );
-// };
 
 const csvUrl =
   "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv";
 
-//   With fetch, async, await -------------------------------------
+export const App = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    csv(csvUrl).then(setData);
+  }, []);
 
-// const fetchText = async (url) => {
-//   const response = await fetch(url);
-//   return await response.text();
-// };
-// fetchText(csvUrl).then((text) => {
-//   const data = d3.csvParse(text);
-//   let message = "";
-//   message = message + Math.round(text.length / 1024) + " kB\n";
-//   message = message + data.length + " rows\n";
-//   message = message + data.columns.length + " columns";
-//   document.querySelector("#message-container").textContent = message;
-// });
-
-// With d3 csv ----------------------------------------------------
-
-d3.csv(csvUrl).then((data) => {
-    console.log(d3.csvFormat(data));
-  let message = "";
-  message = message + Math.round(d3.csvFormat(data).length / 1024) + " kB\n";
-  message = message + data.length + " rows\n";
-  message = message + data.columns.length + " columns";
-  document.querySelector("#message-container").textContent = message;
-});
-export const App = () => <pre id="message-container"></pre>;
+  return <pre>{data ? message(data) : "Loading..."}</pre>;
+};
---
+++ src/message.js
@@ -0,0 +1,11 @@
+
+// import * as d3 from "d3";
+import { csvFormat } from "d3";
+
+export const message = (data) => {
+  let message = "";
+  message = message + Math.round(csvFormat(data).length / 1024) + " kB\n";
+  message = message + data.length + " rows\n";
+  message = message + data.columns.length + " columns";
+  return message;
+};
